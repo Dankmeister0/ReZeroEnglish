d045ea39726a55e21a88402332da5a494e582d6a
Dankmeister <lrzapie@gmail.com>
2026-05-18 14:21:37 -0500

    more cookie fixes

--- bookmark.js
+++ bookmark.js
@@ -1,12 +1,12 @@
 
 const readChapters = document.cookie.split("|");
-for (let i = 0; i < readChapters.length; ++i) {
+for (let i = 1; i < readChapters.length; ++i) {
 	if (readChapters.at(i) == "") continue;
 	const aElem = document.getElementById(readChapters.at(i));
 	aElem.className = "secondary";
 }
 
-if (document.cookie == "") document.cookie = "|";
+if (document.cookie == "") document.cookie = "chapters=|";
 const list = document.getElementById("list").children;
 for (let i = 0; i < list.length; ++i) {
 	const aElem = list.item(i).firstChild;
