52a93d09b1d91351f6d6289f5db72972c47ea213
Dankmeister <lrzapie@gmail.com>
2026-05-18 19:40:05 -0500

    add ex chapters

--- bookmark.js
+++ bookmark.js
@@ -1,18 +1,52 @@
 
-const readChapters = document.cookie.split("|");
-for (let i = 1; i < readChapters.length; ++i) {
-	if (readChapters.at(i) == "") continue;
-	const aElem = document.getElementById(readChapters.at(i));
-	aElem.className = "secondary";
-}
 
 if (document.cookie == "") document.cookie = "chapters=|";
-const list = document.getElementById("list").children;
-for (let i = 0; i < list.length; ++i) {
-	const aElem = list.item(i).firstChild;
-	if (aElem == null) continue;
-	aElem.onclick = () => {
-		if (document.cookie.indexOf(`|${aElem.id}|`) > -1) return;
-		document.cookie = document.cookie + aElem.id + "|";
-	};
+const listEntries = [];
+initListEntries();
+grayOutReadChapters();
+setupHandlers();
+
+function initListEntries() {
+	const list = document.getElementById("list").children;
+	for (let i = 0; i < list.length; ++i) {
+		const aElem = list.item(i).firstChild;
+		if (aElem == null) continue;
+		listEntries.push(aElem);
+	}
+}
+
+function grayOutReadChapters() {
+	const readChapters = document.cookie.split("|");
+	for (let i = 1; i < readChapters.length; ++i) {
+		if (readChapters.at(i) == "") continue;
+		const aElem = document.getElementById(readChapters.at(i));
+		aElem.className = "secondary";
+	}
+}
+
+function setupHandlers() {
+	for (const elem of listEntries) {
+		elem.onclick = () => { readChapter(elem.id); }
+	}
+
+	const readAllButton = document.getElementById("readAll");
+	readAllButton.onclick = () => {
+		for (const elem of listEntries) {
+			readChapter(elem.id);
+			elem.className = "secondary";
+		}
+	}
+
+	const unreadAllButton = document.getElementById("readNone");
+	unreadAllButton.onclick = () => {
+		document.cookie = "chapters=|";
+		for (const elem of listEntries) {
+			elem.className = "";
+		}
+	}
+}
+
+function readChapter(chapter) {
+	if (document.cookie.indexOf(`|${chapter}|`) > -1) return;
+	document.cookie = document.cookie + chapter + "|";
 }
